bc0c4ada1c39d0c58382a835ec516f81b2560d4d
Rajat <rajatmig29@gmail.com>
2015-06-08 17:09:42 -0700

    added framing options

--- Components/framingOptions.react.js
+++ Components/framingOptions.react.js
@@ -15,12 +15,15 @@ var framingOptions = React.createClass({
 				</div>
 				<div className="framingOptions">
 					<div className="unframed">
+					<img src="http://cache1.allpostersimages.com/images/photostoart/print_only_icon.png"/>
 						<span>Unframed Print</span>
 					</div>
 					<div className="recomended">
+						<img src="http://cache1.allpostersimages.com/images/photostoart/switch_art_icon.png"/>
 						<span>Recomended Frame</span>
 					</div>
 					<div className="customFrame">
+					<img src="http://cache1.allpostersimages.com/images/photostoart/services_icons_framing.png"/>
 						<span>Design a Custom Frame</span>
 					</div>
 				</div>
